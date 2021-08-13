53494a728ee345630aa38a1dbc70e4ed1b389795
cannnzhang <cannnzhang@tencent.com>
2021-08-13 13:00:13 +0800

    在线反编译网站 很好用

--- app/src/main/java/com/canzhang/asmdemo/test/JavaCodeTest.java
+++ app/src/main/java/com/canzhang/asmdemo/test/JavaCodeTest.java
@@ -2,6 +2,9 @@ package com.canzhang.asmdemo.test;
 
 /**
  * 字节码相关测试
+ *
+ * 下面这个是个在线编译网站  很好用
+ * https://javap.yawk.at/#NDJVKw
  */
 public class JavaCodeTest {
     private void div(int a, int b) {
